14de07122725cbe4d1d5310a3a6b806f17ac57b5
John Harvey <jharvey@mac.com>
2024-10-24 13:24:45 -0400

    Add the configurations for formatting, gitsigns, linting, lsp, nvim-cmp and trouble.  This is all taken from: https://www.youtube.com/watch?v=6pAG3BHurdM and https://www.josean.com/posts/how-to-setup-neovim-2024

--- nvim/lua/jh/lazy.lua
+++ nvim/lua/jh/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("jh.plugins", { 
+require("lazy").setup({ { import = "jh.plugins" }, { import = "jh.plugins.lsp" } }, { 
   checker = {
     enabled = true,
     notify = false,
--- nvim/lua/jh/plugins/nvim-cmp.lua
+++ nvim/lua/jh/plugins/nvim-cmp.lua
@@ -45,6 +45,7 @@ return {
       }),
 
       sources = cmp.config.sources({
+        { name = "nvim_lsp" },
         { name = "luasnip" },
         { name = "buffer" },
         { name = "path" },
